87f91b32c872fa680f27297690f4502dc7296a55
Mirko Bastianini <mirko.bastianini@gmail.com>
2016-09-21 09:16:53 +0200

    Add files via upload

---
+++ PingScanner/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace PingScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.informazioniSuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.inputNetwork = new System.Windows.Forms.TextBox();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textStatus = new System.Windows.Forms.Label();
+            this.textStatus0 = new System.Windows.Forms.Label();
+            this.listHosts = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.esciToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // esciToolStripMenuItem
+            // 
+            this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
+            this.esciToolStripMenuItem.Size = new System.Drawing.Size(94, 22);
+            this.esciToolStripMenuItem.Text = "Esci";
+            this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.informazioniSuToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "?";
+            // 
+            // informazioniSuToolStripMenuItem
+            // 
+            this.informazioniSuToolStripMenuItem.Name = "informazioniSuToolStripMenuItem";
+            this.informazioniSuToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.informazioniSuToolStripMenuItem.Text = "Informazioni su...";
+            this.informazioniSuToolStripMenuItem.Click += new System.EventHandler(this.informazioniSuToolStripMenuItem_Click);
+            // 
+            // inputNetwork
+            // 
+            this.inputNetwork.Location = new System.Drawing.Point(12, 37);
+            this.inputNetwork.Name = "inputNetwork";
+            this.inputNetwork.Size = new System.Drawing.Size(100, 20);
+            this.inputNetwork.TabIndex = 2;
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Location = new System.Drawing.Point(118, 35);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(75, 23);
+            this.buttonScan.TabIndex = 3;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.buttonScan_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(199, 35);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 4;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.textStatus);
+            this.panel1.Controls.Add(this.textStatus0);
+            this.panel1.Location = new System.Drawing.Point(12, 64);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 53);
+            this.panel1.TabIndex = 5;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 25);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(251, 17);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // textStatus
+            // 
+            this.textStatus.AutoSize = true;
+            this.textStatus.Location = new System.Drawing.Point(60, 9);
+            this.textStatus.Name = "textStatus";
+            this.textStatus.Size = new System.Drawing.Size(24, 13);
+            this.textStatus.TabIndex = 1;
+            this.textStatus.Text = "Idle";
+            // 
+            // textStatus0
+            // 
+            this.textStatus0.AutoSize = true;
+            this.textStatus0.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textStatus0.Location = new System.Drawing.Point(3, 9);
+            this.textStatus0.Name = "textStatus0";
+            this.textStatus0.Size = new System.Drawing.Size(51, 13);
+            this.textStatus0.TabIndex = 0;
+            this.textStatus0.Text = "Status: ";
+            // 
+            // listHosts
+            // 
+            this.listHosts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listHosts.Location = new System.Drawing.Point(12, 123);
+            this.listHosts.Name = "listHosts";
+            this.listHosts.Size = new System.Drawing.Size(257, 126);
+            this.listHosts.TabIndex = 6;
+            this.listHosts.UseCompatibleStateImageBehavior = false;
+            this.listHosts.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP";
+            this.columnHeader1.Width = 89;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Hostname";
+            this.columnHeader2.Width = 104;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listHosts);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.inputNetwork);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PingScanner";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem informazioniSuToolStripMenuItem;
+        private System.Windows.Forms.TextBox inputNetwork;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label textStatus;
+        private System.Windows.Forms.Label textStatus0;
+        private System.Windows.Forms.ListView listHosts;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ PingScanner/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PingScanner
+{
+    public partial class Form1 : Form
+    {
+        IPAddress ipaddress;
+        IPAddress netmask;
+        IPAddress networkaddress;
+        string networkstring;
+        Thread thread = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            ipaddress = GetIPAdress();
+            netmask = GetNetmask();
+            networkaddress = GetNetworkAddress(ipaddress, netmask);
+            networkstring = networkaddress.ToString();
+            networkstring = Regex.Replace(networkstring, ".[^\\.]*$", "");
+            inputNetwork.Text = networkstring;
+            buttonStop.Enabled = false;
+        }
+
+        public IPAddress GetIPAdress()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var addr = ni.GetIPProperties().GatewayAddresses.FirstOrDefault();
+                if (addr != null && !addr.Address.ToString().Equals("0.0.0.0"))
+                {
+                    if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                    {
+                        foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                return ip.Address;
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public IPAddress GetNetmask()
+        {
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                var addr = ni.GetIPProperties().GatewayAddresses.FirstOrDefault();
+                if (addr != null && !addr.Address.ToString().Equals("0.0.0.0"))
+                {
+                    if (ni.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || ni.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
+                    {
+                        foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                        {
+                            if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                return ip.IPv4Mask;
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public IPAddress GetNetworkAddress(IPAddress address, IPAddress subnetMask)
+        {
+            byte[] ipAddressBytes = address.GetAddressBytes();
+            byte[] subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAddressBytes.Length != subnetMaskBytes.Length)
+                MessageBox.Show("Length of ip address and netmask are different !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            byte[] broadcastAddress = new byte[ipAddressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAddressBytes[i] & (subnetMaskBytes[i]));
+            }
+            return new IPAddress(broadcastAddress);
+        }
+
+        private void buttonScan_Click(object sender, EventArgs e)
+        {
+            if (inputNetwork.Text == string.Empty)
+            {
+                MessageBox.Show("Insert the network address that you want to scan !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                thread = new Thread(() => scan(inputNetwork.Text));
+                thread.Start();
+                if (thread.IsAlive)
+                {
+                    buttonStop.Enabled = true;
+                    buttonScan.Enabled = false;
+                    buttonScan.Enabled = false;
+                }
+            }
+        }
+
+        public void scan(String prefix)
+        {
+            try
+            {
+                progressBar1.Maximum = 254;
+                progressBar1.Value = 0;
+                Ping ping;
+                PingReply reply;
+                IPAddress ipaddress;
+                IPHostEntry host;
+                listHosts.Items.Clear();
+                for (int i = 1; i < 255; i++)
+                {
+                    string suffix = "." + i.ToString();
+                    ping = new Ping();
+                    reply = ping.Send(prefix + suffix, 300);
+                    textStatus.ForeColor = System.Drawing.Color.Red;
+                    textStatus.Text = "Scanning " + prefix + suffix;
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        try
+                        {
+                            ipaddress = IPAddress.Parse(prefix + suffix);
+                            host = Dns.GetHostEntry(ipaddress);
+                            listHosts.Items.Add(new ListViewItem(new String[] { prefix + suffix, host.HostName, "Up" }));
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Can't ping " + prefix + suffix + " host !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                    }
+                    progressBar1.Value += 1;
+                }
+                textStatus.ForeColor = System.Drawing.Color.Green;
+                textStatus.Text = "Done";
+                int count = listHosts.Items.Count;
+                MessageBox.Show("Found " + count + " hosts in this LAN !", "Scan terminated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                buttonStop.Enabled = false;
+                buttonScan.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Lan not valid !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            stopWork();
+        }
+
+        public void stopWork()
+        {
+            try
+            {
+                thread.Suspend();
+            }
+            catch
+            {
+                MessageBox.Show("Can't stop thread !", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            buttonScan.Enabled = true;
+            buttonStop.Enabled = false;
+            buttonScan.Enabled = true;
+            progressBar1.Value = 0;
+            textStatus.ForeColor = System.Drawing.Color.Red;
+            textStatus.Text = "Idle";
+        }
+
+        private void esciToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void informazioniSuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("PingScanner v.1.0\n\nDeveloped by: Mirko Bastianini", "Informazioni", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
